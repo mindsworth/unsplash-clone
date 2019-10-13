00230014252cdee06f8af0f315a595701e6601b9
Prince Goziem <chigoziem.nwaiwu@andela.com>
2019-10-13 20:56:23 +0100

    create header section

---
+++ src/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SearchInput from '../components/SearchInput/SearchInput'
+
+const App = () => {
+	return (
+		<div className="app-container">
+			<header className="header">
+				<SearchInput />
+			</header>
+		</div>
+	)
+}
+
+export default App
