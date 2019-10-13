cea79a63794b130f6e3e6c516052051592f38277
Prince Goziem <chigoziem.nwaiwu@andela.com>
2019-10-13 11:10:20 +0100

    setup typscript and base styles

---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const SearchInput = ({}) => (
+    <form action="">
+        <label htmlFor=""></label>
+        <input type="text" name="" id=""/>
+    </form>
+);
+
+export default SearchInput;
