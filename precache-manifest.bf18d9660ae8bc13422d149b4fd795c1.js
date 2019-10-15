29a8eb8dcf6d7edd9c10d9ec540f03355d5dd1ec
Prince Goziem <chigoziem.nwaiwu@andela.com>
2019-10-16 00:02:37 +0100

    Updates

---
+++ precache-manifest.bf18d9660ae8bc13422d149b4fd795c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "491f241cabe2a9a3d5a8feb813175c24",
+    "url": "/unsplash-clone/index.html"
+  },
+  {
+    "revision": "b0921b45b847d4fe05ce",
+    "url": "/unsplash-clone/static/css/main.abd4f41f.chunk.css"
+  },
+  {
+    "revision": "2345994f468af112b45a",
+    "url": "/unsplash-clone/static/js/2.3e9fbd75.chunk.js"
+  },
+  {
+    "revision": "b0921b45b847d4fe05ce",
+    "url": "/unsplash-clone/static/js/main.dae605bc.chunk.js"
+  },
+  {
+    "revision": "a4bc27c99de09e0e2070",
+    "url": "/unsplash-clone/static/js/runtime-main.15a0a076.js"
+  }
+]);
