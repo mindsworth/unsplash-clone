d33a54ac5d4940ee35ec7c7eb2dadc55d90558d3
Prince Goziem <chigoziem.nwaiwu@andela.com>
2019-10-15 11:59:04 +0100

    implement fetch and search photos functionality

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -3,16 +3,20 @@ import React from 'react'
 interface CardProps {
     classes?: string
     imgSrc: string
+    imgDetails: {
+        name: string
+        location: string
+    }
 }
 
-const Card = ({ classes, imgSrc }: CardProps) => (
+const Card = ({ classes, imgSrc, imgDetails }: CardProps) => (
     <div className={`card ${classes}`}>
         <div className="card--image-wrap">
             <img src={imgSrc} alt="Unsplash" />
         </div>
         <div className="card--info">
-            <h4>Chigoziem Nwaiwu</h4>
-            <p>London, United Kingdom</p>
+            <h4>{imgDetails.name}</h4>
+            <p>{ imgDetails.location}</p>
         </div>
     </div>
 )
--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -1,12 +1,37 @@
 import React from 'react'
 
-const SearchInput = () => (
-    <form action="" className="header-form">
+interface SearchInputProps {
+    page: number
+    query: string
+    setQuery: React.Dispatch<React.SetStateAction<string>>
+    setUrl: React.Dispatch<React.SetStateAction<string>>
+    setResultHeader: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const SearchInput = ({
+    page,
+    query,
+    setQuery,
+    setUrl,
+    setResultHeader
+}: SearchInputProps) => (
+    <form
+        onSubmit={event => {
+            event.preventDefault()
+            setUrl(
+                `https://api.unsplash.com/search/photos/?client_id=c30a02c9df167f275d03fca6766b9542b302ca4b0cbc30fbad5dfddd65d99ccf&page=${page}&per_page=11&query=${query}`
+            )
+            setResultHeader(true)
+        }}
+        className="header-form"
+    >
         <button type="submit" className="submit-btn">
             <img src="./search.svg" alt="search-icon" width="20" />
         </button>
         <input
             type="text"
+            value={query}
+            onChange={event => setQuery(event.target.value)}
             className="form-control"
             placeholder="Search for photo"
         />
