99fe49a307f25ef122a66020ae27c5f1ea4064f8
Prince Goziem <chigoziem.nwaiwu@andela.com>
2019-10-15 22:35:35 +0100

    move cliendId to env file

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -9,7 +9,7 @@ const App = () => {
     const [query, setQuery] = useState('')
     const page = Math.floor(Math.random() * 10)
     const [url, setUrl] = useState(
-        `https://api.unsplash.com/search/photos/?client_id=c30a02c9df167f275d03fca6766b9542b302ca4b0cbc30fbad5dfddd65d99ccf&page=${page}&per_page=11&query=african`
+        `https://api.unsplash.com/search/photos/?client_id=${process.env.REACT_APP_CLIENT_ID}&page=${page}&per_page=11&query=african`
     )
     const [isLoading, setIsLoading] = useState(false)
     const [isResultHeader, setResultHeader] = useState(false)
